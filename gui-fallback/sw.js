547e1992738b75d751ee19bb9b8b10d089cc8ab5
xarta-node <xarta-node@xarta.co.uk>
2026-05-25 18:43:03 +0000

    Tune mobile portrait voice controls

--- gui-fallback/sw.js
+++ gui-fallback/sw.js
@@ -1,4 +1,4 @@
-const BP_CACHE_VERSION = 'bp-fallback-v259';
+const BP_CACHE_VERSION = 'bp-fallback-v260';
 const STATIC_CACHE = `${BP_CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${BP_CACHE_VERSION}-runtime`;
 
