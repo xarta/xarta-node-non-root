e96bcbd26eef37ed575e2e806afe1ee25f373d0b
xarta-node <xarta-node@xarta.co.uk>
2026-05-26 07:49:01 +0000

    Ignore password managers on Voice Mode sliders

--- gui-fallback/sw.js
+++ gui-fallback/sw.js
@@ -1,4 +1,4 @@
-const BP_CACHE_VERSION = 'bp-fallback-v268';
+const BP_CACHE_VERSION = 'bp-fallback-v269';
 const STATIC_CACHE = `${BP_CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${BP_CACHE_VERSION}-runtime`;
 
