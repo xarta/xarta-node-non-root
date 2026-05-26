6cd91df4cfed9d4f3996bbc4c45565c4dce46b92
xarta-node <xarta-node@xarta.co.uk>
2026-05-26 07:24:36 +0000

    Refine Voice Mode controls layout

--- gui-fallback/sw.js
+++ gui-fallback/sw.js
@@ -1,4 +1,4 @@
-const BP_CACHE_VERSION = 'bp-fallback-v263';
+const BP_CACHE_VERSION = 'bp-fallback-v266';
 const STATIC_CACHE = `${BP_CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${BP_CACHE_VERSION}-runtime`;
 
